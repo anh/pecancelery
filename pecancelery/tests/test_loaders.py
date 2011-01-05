b94690866a4ce9d5b302cfd2486b924a9415c7e0
Ryan Petrello <lists@ryanpetrello.com>
2011-01-05 17:44:08 -0500

    More unit tests.

---
+++ pecancelery/tests/test_loaders.py
@@ -0,0 +1,17 @@
+from pecancelery    import loader, app
+from celery         import loaders
+
+import unittest
+
+
+class TestPecanLoader(unittest.TestCase):
+    
+    def setUp(self):
+        self.loader = loader.PecanLoader()
+    
+    def test_loader_cls(self):
+        self.assertEqual(loaders.get_loader_cls(loader.LOADER_ALIAS), self.loader.__class__)
+        
+    def test_apploader(self):
+        base_app = app.BaseAppFactory().instance
+        self.assertEqual(base_app.loader.__class__, self.loader.__class__)
