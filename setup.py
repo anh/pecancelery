ab01e92b2d7c6a337878a419850d4306051f21ea
Ryan Petrello <lists@ryanpetrello.com>
2011-02-18 12:06:32 -0500

    Some tinkering w/ setup.py.

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ requirements = [
 setup(
     name                    = "pecancelery",
     version                 = "0.0.1a1",
-    packages                = ['pecancelery'],
     include_package_data    = True,
                             
     # metadata              
