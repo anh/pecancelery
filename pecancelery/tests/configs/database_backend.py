4941df146d01a535415ae78e41a43fcbb1137e5f
Ryan Petrello <lists@ryanpetrello.com>
2011-06-15 12:33:46 -0400

    Fixing a failing unit test (mis-paste on my part).

--- pecancelery/tests/configs/database_backend.py
+++ pecancelery/tests/configs/database_backend.py
@@ -1,5 +1,4 @@
 celery = {
-    'CELERY_IMPORTS'                : ('package.module.tasks',),
     'CELERY_RESULT_BACKEND'         : 'database',
     'CELERY_RESULT_DBURI'           : 'postgresql+psycopg2://user:pass@localhost/table'
 }
